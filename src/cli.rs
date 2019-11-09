d969264407f9da00da7618156f64acb3bc34f184
fprince <fprince1989@gmail.com>
2019-11-10 00:27:58 +0800

    subcommand help args UpperCase

--- src/cli.rs
+++ src/cli.rs
@@ -15,19 +15,23 @@ pub enum Command {
     /// Get the string value of a given string key
     Get {
         /// the string key
+        #[structopt(name = "KEY")]
         key: String,
     },
     /// Set the value of a string key to a string
     Set {
         /// the string key
+        #[structopt(name = "KEY")]
         key: String,
         /// the string value
+        #[structopt(name = "VALUE")]
         value: String,
     },
     /// Remove a given key
     #[structopt(name = "rm")]
     Remove {
         /// the string key
+        #[structopt(name = "KEY")]
         key: String,
     },
 }
