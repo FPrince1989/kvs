cd7eac86563118a3c66f569c1bdc1e61326e2b9e
fprince <fprince1989@gmail.com>
2020-08-09 23:09:57 +0800

    Add benchmark

---
+++ benches/main.rs
@@ -0,0 +1,24 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand::{thread_rng, Rng};
+use tempfile::TempDir;
+
+use kvs::{KvStore, KvsEngine};
+
+fn bench_kvs_set(c: &mut Criterion) {
+    let mut rng = thread_rng();
+    let temp_dir = TempDir::new().unwrap();
+    let mut kvs = KvStore::open(temp_dir.path()).unwrap();
+    // let mut kvs = KvStore::open(&current_dir().unwrap()).unwrap();
+    c.bench_function("kvs_write", move |b| {
+        b.iter(|| {
+            let key = rng.gen_range(1, 1_000_000);
+            let value = rng.gen_range(1, 1_000_000);
+            kvs.set(format!("key{}", key), format!("value{}", value))
+                .unwrap();
+        })
+    });
+}
+
+criterion_group!(benches, bench_kvs_set);
+
+criterion_main!(benches);
